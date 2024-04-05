2af080c5c4aabd421a7f045c8abf3ad91751b671
Yousef Hesham <yousefhesham1235@gmail.com>
2024-04-05 23:10:50 +0200

    Server edited

---
+++ server/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+import util
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/hello")
+def hello():
+    return "hi"
+
+
+@app.route('/classify_image', methods=['GET', 'POST'])
+def classify_image():
+    image_data = request.form['image_data']
+
+    response = jsonify(util.classify_image(image_data))
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    util.load_artifacts()
+    app.run(port=5000)
